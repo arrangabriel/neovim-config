dfa228dcf4ab52f9adf12ff35c8413799d674295
Arran Gabriel <arrankg@gmail.com>
2024-06-18 10:40:07 +0200

    fix markdown formatter

--- lua/plugins/language_support/conform.lua
+++ lua/plugins/language_support/conform.lua
@@ -1,4 +1,4 @@
-local prettier = { { "prettierd", "prettier" } }
+local prettier = "prettier"
 
 return {
 	"stevearc/conform.nvim",
@@ -14,6 +14,7 @@ return {
 		},
 	},
 	opts = {
+		log_level = vim.log.levels.TRACE,
 		notify_on_error = false,
 		format_on_save = function(bufnr)
 			-- Languages that do not have a "default" style
@@ -23,16 +24,22 @@ return {
 				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
 			}
 		end,
+		-- add these to the mason config to ensure their installation
 		formatters_by_ft = {
-			-- add these to the mason config to ensure their installation
-			-- TODO: add more
 			lua = { "stylua" },
-			javascript = prettier,
-			javascriptreact = prettier,
-			typescript = prettier,
-			typescriptreact = prettier,
-			css = prettier,
-			html = prettier,
+			javascript = { prettier },
+			javascriptreact = { prettier },
+			typescript = { prettier },
+			typescriptreact = { prettier },
+			css = { prettier },
+			html = { prettier },
+			markdown = { prettier },
+			["_"] = { "trim_whitespace" },
+		},
+		formatters = {
+			prettier = {
+				append_args = { "--prose-wrap", "always" },
+			},
 		},
 	},
 }
