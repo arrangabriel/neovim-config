db2ebede428488a4316cdc340907ccbec1dee3f9
Arran Gabriel <arrankg@gmail.com>
2024-11-10 00:11:20 +0100

    Update which-key config

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -2,15 +2,22 @@
 local which_key_config = {
 	"folke/which-key.nvim",
 	event = "VimEnter",
-	config = function()
-		local which_key = require("which-key")
-		which_key.setup()
-
-		which_key.register({
-			["<leader>t"] = { name = "[t]oggle", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[s]earch", _ = "which_key_ignore" },
-		})
-	end,
+	opts = {
+		spec = {
+			{ "<leader>s", group = "[s]earch" },
+			{ "<leader>t", group = "[t]oggle" },
+			{ "<leader>g", group = "[g]oto" },
+		},
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "view buffer local keymaps",
+		},
+	},
 }
 
 -- Various utility plugins
