b773a610fc12211c88c9f8232b34d6bf435131b1
Arran Gabriel <arrankg@gmail.com>
2024-11-10 13:11:11 +0100

    Add utils documentation

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+---@param prefix string
+---@param opts vim.keymap.set.Opts?
 M.map_with_prefix = function(prefix, opts)
 	local actual_opts = opts or {}
 	return function(mode, keys, action, desc)
@@ -8,10 +10,17 @@ M.map_with_prefix = function(prefix, opts)
 	end
 end
 
+---@param mode string|string[]
+---@param keys string
+---@param action string|function
+---@param opts vim.keymap.set.Opts?
 M.map = function(mode, keys, action, opts)
 	vim.keymap.set(mode, keys, action, opts or {})
 end
 
+---@param mode string|string[]
+---@param keys string
+---@param action string|function
 M.map_silent = function(mode, keys, action)
 	M.map(mode, keys, action, { silent = true })
 end
